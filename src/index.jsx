30e55c2c4a465ea8a8754f4825b299039c50c352
James Wordie <james.wordie@yahoo.com>
2021-06-23 21:49:26 +0100

    working no class components and geolocation

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './components/season-display';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      error => {
+        this.setState({ errorMessage: error.message });
+      }
+    )
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
